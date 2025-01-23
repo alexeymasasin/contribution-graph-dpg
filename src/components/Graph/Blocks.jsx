2deb4caf31baa2cabe9544e4c42ae4d9a00aebe8
Alexey Masasin <alexeymasasin@gmail.com>
2025-01-23 14:31:42 +0500

    Weekdays and months display

--- src/components/Graph/Blocks.jsx
+++ src/components/Graph/Blocks.jsx
@@ -72,36 +72,93 @@ export default function Blocks() {
 
 	const sortedDates = getSortedDates(dates);
 
+	const getWeekdayLabels = () => {
+		const weekdays = ['Пн', 'none', 'Ср', 'none', 'Пт', 'none', 'none'];
+		return weekdays.map((day, index) => (
+			<p key={index} className="single-weekday">
+				{day !== 'none' ? day : ' '}
+			</p>
+		));
+	};
+
+	const getMonthLabels = (dates) => {
+		const monthNames = {
+			'01': 'Янв.',
+			'02': 'Февр.',
+			'03': 'Март',
+			'04': 'Апр.',
+			'05': 'Май',
+			'06': 'Июнь',
+			'07': 'Июль',
+			'08': 'Авг.',
+			'09': 'Сент.',
+			10: 'Окт.',
+			11: 'Нояб.',
+			12: 'Дек.',
+		};
+
+		const monthLabels = [];
+		const uniqueMonths = new Set();
+
+		for (let i = 0; i < dates.length; i++) {
+			const date = dates[i];
+			if (date) {
+				const monthYear = date.toISOString().split('T')[0].slice(0, 7);
+				if (!uniqueMonths.has(monthYear)) {
+					uniqueMonths.add(monthYear);
+					const month = date.toISOString().split('T')[0].slice(5, 7);
+					monthLabels.push(monthNames[month]);
+				}
+			}
+		}
+
+		return monthLabels.slice(-12).map((month, index) => (
+			<p key={index} className="single-month">
+				{month}
+			</p>
+		));
+	};
+
+	const sortedMonthDates = getSortedDates(dates);
+	const monthLabels = getMonthLabels(sortedMonthDates);
+
 	return (
-		<div className="blocks">
-			{[...Array(51)].map((_, weekIndex) => (
-				<div key={weekIndex} className="column">
-					{[...Array(7)].map((_, dayIndex) => {
-						const index = weekIndex * 7 + dayIndex;
-						const date = sortedDates[index];
-						return (
-							<div
-								key={index}
-								className={`day ${
-									date
-										? getColor(
-												contributions[date.toISOString().split('T')[0]] || 0
-										  )
-										: 'gray'
-								}`}
-								tabIndex={0}
-								title={
-									date
-										? `${date.toISOString().split('T')[0]}: ${
-												contributions[date.toISOString().split('T')[0]] || 0
-										  } commits`
-										: ''
-								}
-							/>
-						);
-					})}
-				</div>
-			))}
-		</div>
+		<>
+			<div className="months">
+				<div className="single-month" />
+				{monthLabels}
+			</div>
+			<div className="blocks">
+				<div className="weekdays">{getWeekdayLabels()}</div>
+				{[...Array(51)].map((_, weekIndex) => (
+					<div key={weekIndex} className="column">
+						{[...Array(7)].map((_, dayIndex) => {
+							const index = weekIndex * 7 + dayIndex;
+							const date = sortedDates[index];
+							return (
+								<div
+									key={index}
+									className={`day ${
+										date
+											? getColor(
+													contributions[date.toISOString().split('T')[0]] || 0
+											  )
+											: 'gray'
+									}`}
+									tabIndex={0}
+									title={
+										date
+											? `${date.toISOString().split('T')[0]}: ${
+													contributions[date.toISOString().split('T')[0]] || 0
+											  } commits`
+											: ''
+									}
+								/>
+							);
+						})}
+					</div>
+				))}
+			</div>
+		</>
 	);
 }
